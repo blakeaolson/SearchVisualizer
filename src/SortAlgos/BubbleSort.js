34d346123249713659177975eaa9678782ce89b3
blakeaolson <blakeaolson1@gmail.com>
2023-02-13 12:55:01 -0600

    improved efficiency of bubblesort

--- src/SortAlgos/BubbleSort.js
+++ src/SortAlgos/BubbleSort.js
@@ -1,12 +1,11 @@
 export default function BubbleSort(heightVals) {
   const n = heightVals.length
-  
   for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n; j++) {
-      if (j !== n - 1 && heightVals[j + 1] < heightVals[j]) {
-        let temp = heightVals[j + 1];
-        heightVals[j + 1] = heightVals[j];
-        heightVals[j] = temp;
+    for (let j = 0; j < i; j++) {
+      if (heightVals[i] < heightVals[j]) {
+        let temp = heightVals[j];
+        heightVals[j] = heightVals[i];
+        heightVals[i] = temp;
       }
     }
   }
